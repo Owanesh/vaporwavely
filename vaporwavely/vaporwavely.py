ba60883e92857d778bbef9a6dd1601c5010f1e2e
Meridien <daniel.mark.spencer@gmail.com>
2018-10-28 15:57:04 +1000

    Added supported terminal programs to README.md and also added some comments to vaporwavely.py

--- vaporwavely/vaporwavely.py
+++ vaporwavely/vaporwavely.py
@@ -13,6 +13,7 @@ def vaporize(payload):
         if ord('!') <= pos_norm_char <= ord('~'):
             # these two char are limit of alphabet
             char = chr(pos_norm_char + alphabet_distance)
+        #shift payload char from normal alphabet to vaporwave alphabet
         new_payload += char
     return new_payload
     # thanks to https://github.com/jonesmartins/vapyrwave/
@@ -20,9 +21,12 @@ def vaporize(payload):
 
 
 def vaporipsum(number_of_paragraph=1):
+    #defaults to 1 paragraph
     vaporipsum_final = ' '
     for _ in range(number_of_paragraph):
+        #generate paragraph
         paragraph = random.sample(dictionary, len(dictionary))
+        #join paragraphs separated by new lines
         vaporipsum_final += ' '.join(paragraph) + '\n\n'
 
     return vaporipsum_final
